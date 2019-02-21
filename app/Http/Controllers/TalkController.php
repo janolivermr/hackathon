dae277282b73d9cd834a083f4f8745f5cb6a20b2
Jan Stolle <j.stolle@criteo.com>
2019-02-21 20:34:24 +0000

    Add Phone Number and Talks retrieval

---
+++ app/Http/Controllers/TalkController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Carbon\Carbon;
+use GuzzleHttp\Client;
+use Illuminate\Http\Request;
+
+class TalkController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $http = new Client();
+        $res = $http->get('http://api.joind.in/v2.1/events/7001/talks?resultsperpage=100')->getBody()->getContents();
+        $talks = collect(json_decode($res)->talks);
+        $talks = $talks->filter(function ($talk) {
+            return !in_array($talk->tracks[0]->track_name, [
+                'Workshop Track',
+                'Social',
+            ]);
+        });
+        $talks->map(function ($talk) {
+            $talk->start_time = new Carbon($talk->start_date);
+            $talk->end_time = $talk->start_time->copy();
+            $talk->end_time->addMinutes($talk->duration);
+            return $talk;
+        });
+//        dd($talks);
+        return view('talks.index', compact('talks'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ resources/views/talks/index.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <table class="table table-striped">
+                <thead>
+                <tr>
+                    <th>Date/Time</th>
+                    <th>Talk</th>
+                    <th>Track</th>
+                    <th>Match me!</th>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach($talks as $talk)
+                    <tr>
+                        <td> {{$talk->start_time->format('Y-m-d H:i')}} </td>
+                        <td> {{$talk->talk_title}} </td>
+                        <td> {{$talk->tracks[0]->track_name}} </td>
+                        <td> {{$talk->stub}} </td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+@endsection
